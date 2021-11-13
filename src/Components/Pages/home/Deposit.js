15cff64bf7c719add736ab6301e9b48b50fdb15b
AradhanaChhabra <aradhanachhabra03@gmail.com>
2021-11-13 21:28:59 +0530

    deposit page added and css updated

--- src/Components/Pages/home/Deposit.js
+++ src/Components/Pages/home/Deposit.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import './deposit.css'
+import Paper from '@mui/material/Paper';
 
 const Deposit = () => {
+    const addDepositHandler = (event) => {
+        event.preventDefault();
+    }
     return (
         <>
-        <div>
-            Add new Deposit
+            <div className="expenseContainer">
+                <h1 className="exph1">Deposit</h1>
+                <hr className="horizontal"/><br />
+                <Paper elevation={0}>
+                <div className="expform">
+                    <div className="oneline">
+                        <div className="form-field">
+                            <label className="explabel" htmlFor="payee">Title</label>
+                            <input className="expinput" type="text" name="payee" placeholder="Received from"/>
+                        </div>
+                        <div className="form-field">
+                            <label className="explabel" htmlFor="date">Date</label>
+                            <input className="expinput" type="date" name="date"  placeholder="Date"/>
+                        </div>
+                        <div className="form-field">
+                            <label className="explabel" htmlFor="Amount">Amount</label>
+                            <input className="expinput" type="number" />
+                        </div>
+                    </div>
+                    <div className="oneline">
+                        <div className="form-field">
+                            <label className="explabel" htmlFor="memo">Memo</label>
+                            <input className="expinput" type="text" placeholder="A brief message" />
+                        </div>
+                        <div className="form-field">
+                            <label className="explabel" htmlFor="Attachments">Attachments</label>
+                            <input className="expinput" type="file" name="attachments"/>
+                        </div>
+                    </div>
+                </div>
+                </Paper>
+                <button type="submit" onClick={addDepositHandler} className="expbtn">Add Deposit</button>
         </div>
         </>
     )
